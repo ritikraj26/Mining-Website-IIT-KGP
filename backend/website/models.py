b3cacc073e709b7600d1a6db86050c576824ab73
Ritik Raj <ritikraj2699@outlook.com>
2023-06-13 12:53:36 +0530

    created faculty members and  research topics table

--- backend/website/models.py
+++ backend/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class research_topics(models.Model):
+    topic=models.CharField(max_length=100)
+    descriprion=models.CharField(max_length=500)
+
+class faculty_members(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    email_id=models.EmailField(max_length=200)
+    research_interest=models.ForeignKey(research_topics,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='images')
