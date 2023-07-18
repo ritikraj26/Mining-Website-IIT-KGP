6a3074da67854a1c46e51dd227469e0883809114
Ritik Raj <ritikraj2699@outlook.com>
2023-07-19 00:24:27 +0530

    edited admin panel

--- backend/website/models.py
+++ backend/website/models.py
@@ -4,6 +4,13 @@ class research_area(models.Model):
     topic=models.CharField(max_length=100)
     description=models.CharField(max_length=500)
 
+    class Meta:
+        verbose_name = 'Research Area'
+        verbose_name_plural = 'Research Areas'
+    
+    def __str__(self):
+        return self.topic
+
 
 class faculty_member(models.Model):
     name = models.CharField(max_length=100)
@@ -14,14 +21,28 @@ class faculty_member(models.Model):
     email_id=models.EmailField(max_length=100)
     image=models.ImageField(upload_to="faculty_members", blank=True)
 
+    class Meta:
+        verbose_name = 'Faculty Member'
+        verbose_name_plural = 'Faculty Members'
+    
+    def __str__(self):
+        return self.name
 
+    
 class staff_member(models.Model):
     name=models.CharField(max_length=100)
     lab=models.CharField(max_length=100)
     email_id=models.EmailField(max_length=100,blank=True)
     image=models.ImageField(upload_to="staff_members", blank=True)
 
+    class Meta:
+        verbose_name = 'Staff Member'
+        verbose_name_plural = 'Staff Members'
+    
+    def __str__(self):
+        return self.name
 
+  
 class research_scholar(models.Model):
     name=models.CharField(max_length=100)
     research_area=models.CharField(max_length=100)
@@ -30,6 +51,12 @@ class research_scholar(models.Model):
     email_id=models.EmailField(max_length=100,blank=True)
     image=models.ImageField(upload_to="research_scholars", blank=True)
 
+    class Meta:
+        verbose_name = 'Research Scholar'
+        verbose_name_plural = 'Research Scholars'
+    
+    def __str__(self):
+        return self.name
 
 
 class event(models.Model):
@@ -38,25 +65,58 @@ class event(models.Model):
     concluded = models.BooleanField(default=False)
     dated_at = models.DateField(blank=True,null=True)
 
+    class Meta:
+        verbose_name = 'Event'
+        verbose_name_plural = 'Events'
+    
+    def __str__(self):
+        return self.name
+
 
 class lab(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=100,blank=True)
-    
 
+    class Meta:
+        verbose_name = 'Laboratory'
+        verbose_name_plural = 'Laboratories'
+    
+    def __str__(self):
+        return self.name
+    
 
 class gallery(models.Model):
     image = models.ImageField(upload_to="gallery")
 
+    class Meta:
+        verbose_name = 'Image'
+        verbose_name_plural = 'Images'
+    
+    def __str__(self):
+        return self.name
 
 
 class education(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100,blank=True)
 
+    class Meta:
+        verbose_name = 'Education'
+        verbose_name_plural = 'Education'
+    
+    def __str__(self):
+        return self.name
+
 
 class society(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100,blank=True)
-    link = models.CharField(max_length=100,blank=True) 
+    link = models.CharField(max_length=100,blank=True)
+
+    class Meta:
+        verbose_name = 'Society'
+        verbose_name_plural = 'Societies'
+    
+    def __str__(self):
+        return self.name
 
