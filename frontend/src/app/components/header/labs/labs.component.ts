2f247525189dede712d7198218a81a74359163ff
miningdepiitkgp <website.mi.iitkgp@gmail.com>
2025-02-25 18:20:08 +0530

    Minor Changes

--- frontend/src/app/components/header/labs/labs.component.ts
+++ frontend/src/app/components/header/labs/labs.component.ts
@@ -1,20 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   templateUrl: './labs.component.html',
-  styleUrls: ['./labs.component.css']
+  styleUrls: ['./labs.component.css'],
 })
-export class LabsComponent {
-  constructor(private http:HttpClient) {}
+export class LabsComponent implements OnInit {
+  research_areas: any[] = []; // To hold the list of research areas
+  expandedStates: boolean[] = []; // To track expanded state of each research area
 
-  research_areas:any;
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.research_areas = this.http.get("http://127.0.0.1:8000/home/research_area/").subscribe(
-      data => this.research_areas = data
-    )
+  // Toggle the expansion of the specific research area
+  toggleExpand(index: number) {
+    this.expandedStates[index] = !this.expandedStates[index];
   }
-  // const faculty = this.research_areas.research_area.faculties
+
+  // On component initialization, fetch the research areas from the API
+  ngOnInit() {
+    this.http.get<any[]>('http://127.0.0.1:8000/home/research_area/').subscribe(
+      (data) => {
+        this.research_areas = data;
+        // Initialize the expandedStates array with false (all collapsed initially)
+        this.expandedStates = new Array(this.research_areas.length).fill(false);
+      },
+      (error) => {
+        console.error('Error fetching research areas:', error);
+      }
+    );
+  }
 }
